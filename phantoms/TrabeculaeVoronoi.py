cbaa72e77638be819306e92459bdb6fd616b7e4b
Qian Cao <qcao.dev@gmail.com>
2021-04-06 12:02:19 -0400

    findUniqueEdgesAndFaces

--- phantoms/TrabeculaeVoronoi.py
+++ phantoms/TrabeculaeVoronoi.py
@@ -1,14 +1,18 @@
 """
 Phantoms.TrabeculaeVoronoi
 
-A voronoi-seeding-based trabecular bone phantom. Refactored version
+A voronoi-seeding-based trabecular bone phantom.
+
+Refactored April 2021.
 
 Authors:  Qian Cao, Xin Xu, Nada Kamona, Qin Li
 
 """
 
 import numpy as np
-from scipy.spatial import Voronoi, Delunay
+# Delunary to test for colinearlity
+from scipy.spatial import Voronoi, Delaunay
+# import optimesh # May be a good idea for CVT post perturbation
 import utils
 
 def makeSeedPointsCartesian(Sxyz, Nxyz):
@@ -121,13 +125,16 @@ def applyVoronoi(ppoints, Sxyz):
             continue
         break
     
+    # TODO Centroidal Voronoi Tessellation
+    # https://github.com/nschloe/optimesh (Du et al)
+    
     # Extract index of Voronoi vertices within the VOI extent
     Sxyz = np.array(Sxyz)
     ind = np.nonzero(np.prod(np.abs(vor.vertices) < Sxyz/2, axis=1))[0]
     
     return vor, ind
 
-def findUniqueEdges(vor, ind, maxEdgePerFace=8):
+def findUniqueEdgesAndFaces(vor, ind, maxEdgePerFace=8):
     """
     Compute unique edges for each Voronoi cell
 
@@ -135,6 +142,7 @@ def findUniqueEdges(vor, ind, maxEdgePerFace=8):
     ----------
     vor : scipy.spatial.qhull.Voronoi
         Voronoi tessellation object.
+        https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.Voronoi.html
     ind : np.ndarray integers
         Index of vertices within VOI.
     maxEdgePerFace : integer
@@ -146,29 +154,51 @@ def findUniqueEdges(vor, ind, maxEdgePerFace=8):
 
     """
     
+    # iterate through list and picks out pairs of adjacent elements
+    def roll2(data_list):
+        return [[data_list[x],data_list[x+1]] for x in range(len(data_list)-1)]
+        
+    # Filter data_list and keep only unique elements (which are also lists)
+    def uniq(data_list):
+        # https://stackoverflow.com/questions/3724551/python-uniqueness-for-list-of-lists
+       return [list(x) for x in set(tuple(x) for x in data_list)]
+        
     # Vertices which have been excluded (outside the extent of VOI)
     ind_exclude = set(np.arange(len(vor.vertices)))-set(np.array(ind))
+    ind_exclude.add(-1) # -1 means Vornoi vertex outside the Voronoi Diagram
     
-    # Exclude ridges (collection of vertices) containing excluded vertices
-    vertices = [vor.ridge_vertices[x] for x in len(vor.ridge_vertices) 
-                if vor.ridge_vertices[x]]
+    # Exclude ridges ("faces", list of list of Voronoi vertices) containing excluded vertices
+    ridge_ind = [x for x in range(len(vor.ridge_vertices))
+                if not bool(set(vor.ridge_vertices[x]) & set(ind_exclude))]
+    ridge = [vor.ridge_vertices[x] for x in ridge_ind]
+    # ridge_numverts = [len(vor.ridge_vertices[x]) for x in ridge_ind] # DEBUG only
+
+    # TODO: Condense vertices with coplanar vertices (most faces)
+    # See https://www.mathworks.com/matlabcentral/fileexchange/24484-geom3d
+    # varargout = mergeCoplanarFaces(nodes, varargin)
+    # This may not be necessary
     
-    # Exclude coplanar vertices, get unique facces and vertices
+    # Extract edges from faces
+    ridge_circ = [vor.ridge_vertices[x] + [vor.ridge_vertices[x][0]] 
+                  for x in ridge_ind] # This is a copy of ridge
     
+    
+    # Apply uniqueness to faces and edges
+    uniqueFaces = uniq(ridge)
+    uniqueEdges = []
     
     return uniqueEdges, uniqueFaces
-    
 
 
 if __name__ == "__main__":
-    # TODO move example driver script into here
     
     print('Running example for TrabeculaeVoronoi')
-    Sxyz, Nxyz = (6,6,6), (3,3,3)
+    Sxyz, Nxyz = (10,10,10), (5,5,5)
     Rxyz = 0.5
     
     points = makeSeedPointsCartesian(Sxyz, Nxyz)
     ppoints = perturbSeedPointsCartesianUniformXYZ(points, Rxyz, randState=123)
     vor, ind = applyVoronoi(ppoints, Sxyz)
+    uniq_edges, uniq_faces = findUniqueEdgesAndFaces(vor, ind)
     
     
