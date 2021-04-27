13e57dfebde2ebb0a7314c6eeeda46a72e833605
Qian Cao <qcao.dev@gmail.com>
2021-04-27 01:02:44 -0400

    created Metrics package

---
+++ FEA/utils.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+FEA.utils
+
+Utilities module for finite element analysis
+
+Author:  Qian Cao
+
+"""
+
+import numpy as np
+import pychrono as chrono
+
+
+def Voxel2HexaMesh(volume, voxelSize=(1,1,1), origin=None):
+    # origin: (0,0,0) corresponds to "top left corner", defaults to None (center of volume)
+    pass
+
+def Voxel2SurfMesh(volume, voxelSize=(1,1,1), origin=None):
+    pass
+
+def Surf2TetMesh(surface):
+    pass
---
+++ Metrics/TBS.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Metrics.TBS
+
+Trabecular Bone Score for Projection Images
+
+Author:  Qian Cao
+
+Based on: 
+
+"""
+
+def computeTBS(image, pixelSize=(1,1,1)):
+    # image np.ndarray
+    # pixelSize (mm)
+    
+    
+    
+    pass
