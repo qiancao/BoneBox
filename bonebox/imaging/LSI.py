54f679b0b670fafa99581600c9e95c32352b5fa3
Qian Cao <qcao.dev@gmail.com>
2021-05-03 14:13:35 -0400

    getting started on imaging.LSI

---
+++ bonebox/imaging/LSI.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+imaging.LSI
+
+Linear Shift-invariant Imaging Models of Blur and Noise
+
+Author:  Qian Cao
+
+"""
+
+import numpy as np
+
+def makeGaussMTF2D():
+    pass
+
+def makeGaussNPS2D():
+    pass
+
+def makeGaussMTF3D():
+    pass
+
+def makeApodizedGaussNPS3D():
+    pass
+
+def applyMTF2D(image, F, MTF):
+    pass
+
+def applyNPS2D(image, F, MTF, randState):
+    pass
+
+def applyMTF3D(image, F, MTF):
+    pass
+
+def applyNPS3D(image, F, MTF, randState):
+    pass
+
