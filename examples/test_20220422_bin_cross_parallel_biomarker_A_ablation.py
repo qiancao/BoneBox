8939d26b1335607489d3fee4d5dfaa386102661e
Cao <qian.cao@didsr-wk46-046.fda.gov>
2022-06-02 17:38:32 -0400

    commit
    ablation for texture

--- examples/test_20220422_bin_cross_parallel_biomarker_A_ablation.py
+++ examples/test_20220422_bin_cross_parallel_biomarker_A_ablation.py
@@ -52,11 +52,19 @@ if __name__ == "__main__":
     r2Test = np.zeros((features.shape[2],features.shape[3],features.shape[4]))
     importances = np.zeros((features.shape[1],features.shape[2],features.shape[3],features.shape[4]))
     # remember to save y_test as well, this is constant throughout the script
+
+    #%% Run through all scenarios (BMD metrics only)
     
-    # for cind in range(features.shape[2]): # imaging condition
+    nameList = ["_firstorder_10Percentile","_firstorder_90Percentile","_firstorder_Maximum",
+                "_firstorder_Mean","_firstorder_Median"
+                ]
     
+    names_filtered, features_filtered = getFeaturesByName(nameList,featureNames,features)
     
-    #%% Run through all scenarios
+    # Training and testing scores
+    y_preds = np.zeros((num_test,features_filtered.shape[2],features_filtered.shape[3],features_filtered.shape[4]))
+    r2Test = np.zeros((features_filtered.shape[2],features_filtered.shape[3],features_filtered.shape[4]))
+    importances = np.zeros((features_filtered.shape[1],features_filtered.shape[2],features_filtered.shape[3],features_filtered.shape[4]))
     
     for indNoise, nscale in enumerate(nScales):
         for indResolution, rscale in enumerate(rScales):
@@ -66,7 +74,7 @@ if __name__ == "__main__":
             for sind in range(features.shape[4]): # seed, instance
                 
                 # feature
-                feat = features[:,:,indNoise,indResolution,sind]
+                feat = features_filtered[:,:,indNoise,indResolution,sind]
                 
                 # data and target
                 X = feat
@@ -89,18 +97,18 @@ if __name__ == "__main__":
                 r2Test[indNoise,indResolution,sind] = np.corrcoef(y_pred, y_test)[0,1]**2
                 importances[:,indNoise,indResolution,sind] = rf_pipe['reg'].feature_importances_
                 
-                # Correlation plot
-                plt.ioff()
-                plt.figure()
-                plt.plot(y_test, y_pred,'b.')
-                plt.plot(*(np.linspace(0,np.max(y)),)*2,'k--')
-                plt.xlabel("True")
-                plt.ylabel("Predicted")
-                plt.xlim([0,np.max(y)])
-                plt.ylim([0,np.max(y)])
-                plt.title(f"r2: {r2Test[indNoise,indResolution,sind]} Noise: {indNoise}, Resolution: {indResolution}, Instance: {sind}")
-                plt.savefig(outDir+f"correlation_{indNoise}_{indResolution}_{sind}.png")
-                plt.close("all")
+                # # Correlation plot
+                # plt.ioff()
+                # plt.figure()
+                # plt.plot(y_test, y_pred,'b.')
+                # plt.plot(*(np.linspace(0,np.max(y)),)*2,'k--')
+                # plt.xlabel("True")
+                # plt.ylabel("Predicted")
+                # plt.xlim([0,np.max(y)])
+                # plt.ylim([0,np.max(y)])
+                # plt.title(f"r2: {r2Test[indNoise,indResolution,sind]} Noise: {indNoise}, Resolution: {indResolution}, Instance: {sind}")
+                # plt.savefig(outDir+f"correlation_{indNoise}_{indResolution}_{sind}.png")
+                # plt.close("all")
                 
             np.save(outDir+"y_preds",y_preds)
             np.save(outDir+"y_test",y_test)
@@ -118,7 +126,7 @@ if __name__ == "__main__":
     
     fig = plt.figure(figsize=(7,8))
     cax = fig.axes
-    im = plt.imshow(np.mean(r2Test,axis=2))
+    im = plt.imshow(np.mean(r2Test,axis=2),vmin=0.855,vmax=0.943)
     plt.xlabel("Resolution")
     plt.ylabel("Noise Level")
     plt.title("r2 mean")
@@ -135,31 +143,34 @@ if __name__ == "__main__":
     
     #%% feature importances
     
-    for ind in range(importances.shape[0]):
-        print(ind)
+    outDir0 = "/gpfs_projects/qian.cao/BoneBox-out/test_20220422_bin_cross_parallel_biomarker/"
+    r2Test0 = np.load(outDir0+"r2Test.npy")
+    
+    # for ind in range(importances.shape[0]):
+    #     print(ind)
         
-        img = importances[ind,:,:,:]
-        fn = featureNames[ind]
+    #     img = importances[ind,:,:,:]
+    #     fn = featureNames[ind]
         
-        plt.ioff()
+    plt.ioff()
+    
+    fig = plt.figure(figsize=(7,8))
+    cax = fig.axes
+    im = plt.imshow(np.mean(r2Test0,axis=2)/np.mean(r2Test,axis=2),vmin=0.9,vmax=1.1,cmap="bwr")
+    plt.xlabel("Resolution")
+    plt.ylabel("Noise Level")
+    plt.title("r2 mean ratio")
+    plt.colorbar()
+    plt.savefig(outDir+"fig-r2-mean-ratio-all-vs-bmd.png")
         
-        fig = plt.figure(figsize=(7,8))
-        cax = fig.axes
-        im = plt.imshow(np.mean(img,axis=2),cmap="YlGn")
-        plt.xlabel("Resolution")
-        plt.ylabel("Noise Level")
-        plt.title(f"Importance Mean: {fn}")
-        plt.colorbar()
-        plt.savefig(outDir+f"fig-imp-{fn}-mean.png")
+    #     plt.figure(figsize=(7,8))
+    #     plt.imshow(np.std(img,axis=2),cmap="BuPu")
+    #     plt.xlabel("Resolution")
+    #     plt.ylabel("Noise Level")
+    #     plt.title(f"Importance Std: {fn}")
+    #     plt.colorbar()
+    #     plt.savefig(outDir+f"fig-imp-{fn}-std.png")
         
-        plt.figure(figsize=(7,8))
-        plt.imshow(np.std(img,axis=2),cmap="BuPu")
-        plt.xlabel("Resolution")
-        plt.ylabel("Noise Level")
-        plt.title(f"Importance Std: {fn}")
-        plt.colorbar()
-        plt.savefig(outDir+f"fig-imp-{fn}-std.png")
-        
-        plt.close("all")
+    #     plt.close("all")
         
     #%% 
