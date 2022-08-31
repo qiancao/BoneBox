13d94b68b99e0c1a915f2a57470be6f2babe8ef5
Qian Cao <QianCao227@gmail.com>
2022-08-31 15:20:37 -0400

    TODO: clean out examples

--- bonebox/metrics/Tensor.py
+++ bonebox/metrics/Tensor.py
@@ -125,3 +125,18 @@ if __name__ == "__main__":
     GSTNP = GradientStructureTensor(I)
     print(f"cupy version: \n {GST}")
     print(f"numpy version: \n {GSTNP}")
+
+    # Evaluate GSDT for an anisotropic image
+    from scipy import ndimage
+    np.random.seed(0)
+    I = np.random.rand(200,200,200)
+    Ifilt = ndimage.gaussian_filter(I, [1,2,3])
+    GST = GradientStructureTensorCUDA(Ifilt)
+    print(f"cupy version: \n {GST}")
+
+    import matplotlib.pyplot as plt
+    fig, axs = plt.subplots(1,2,figsize=(10, 4))
+    axs[0].imshow(Ifilt[:,100,:].T,cmap="gray")
+    axs[0].axis("off")
+    axs[1].imshow(Ifilt[100,:,:].T,cmap="gray")
+    axs[1].axis("off")
---
+++ bonebox/utils/gaussian.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+# TODO: Multivariant Gaussian
+def Gaussian(x, mean, cov):
+    pass
